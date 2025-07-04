ef41f7f9ab2cc39e1baa0b95a1858409383ef359
肖尧 <2045872352@qq.com>
2025-07-04 16:03:30 +0800

    初始化

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+
+
+// import { BrowserRouter } from "react-router-dom";
+
+// import {About,Contact,Experience,Feedbacks,Hero,Navbar,StarsCanvas,Tech,Works} from "./components";
+
+
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <div className="relative z-0 bg-primary">
+//         <Navbar />
+//         <Hero />
+//         <About />
+//         <Tech />
+//         <Experience />
+//         <Works />
+//         <Feedbacks />
+//         <Contact />
+//         <StarsCanvas/>
+//         {/* Footer */}
+//         <div className="absolute bottom-10 w-full flex justify-center items-center">
+//           <p className="text-secondary text-[14px]">© 2023 Adrian. All rights reserved.</p>
+//         </div>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
